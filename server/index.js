45c9d0ff1c6742dde2d0b279fe5b0950ba93b7bc
Himanshu Kumar <kumarh664@gmail.com>
2024-02-08 17:26:04 +0530

    setup server boilerplate

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const openaiRoutes = require('./routes/openai');
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// ROUTES
+app.use("/openai", openaiRoutes);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
