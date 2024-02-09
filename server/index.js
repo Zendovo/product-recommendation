add1ac2409286abb716f7558b11a3a9ff5c91d21
Himanshu Kumar <kumarh664@gmail.com>
2024-02-10 02:13:54 +0530

    dockerize and github actions

--- server/index.js
+++ server/index.js
@@ -6,12 +6,12 @@ const port = 3000;
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+    res.send('Hello World!');
 });
 
 // ROUTES
-app.use("/openai", openaiRoutes);
+app.use('/openai', openaiRoutes);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening on port ${port}`);
 });
