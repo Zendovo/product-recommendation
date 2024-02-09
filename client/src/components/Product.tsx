78437fb3d5da23e801f0ff8b88dbfb5f3ff8dec8
Himanshu Kumar <kumarh664@gmail.com>
2024-02-10 04:13:11 +0530

    create basic ui design

---
+++ client/src/components/Product.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+export default function () {
+  return (
+    <div className="p-3 flex flex-col justify-items-start text-left bg-black rounded-lg border-2 mt-4">
+      <div className="text-xl">Great Product</div>
+      <div className="text-lg">- a great point</div>
+      <div className="text-lg">- a great point</div>
+    </div>
+  );
+}
--- client/src/root.tsx
+++ client/src/root.tsx
@@ -1,5 +1,27 @@
 import * as React from 'react';
+import Product from './components/Product';
 
 export default function () {
-  return <div className='mt-10'>Hello World!</div>;
+  return (
+    <div className="bg-black h-screen text-white">
+      <div className="container m-auto flex flex-col justify-center justify-items-center text-center py-32">
+        <div className="text-5xl">Product Recommendation App</div>
+        <div className="flex justify-around">
+          <input
+            className="mt-10 rounded-xl px-4 py-4 min-w-[500px] bg-black border-2 glass"
+            type="text"
+            placeholder="enter an idea..."
+          />
+        </div>
+        <div className="flex justify-around mt-6">
+          <div className="flex flex-col glass mt-2 min-w-[500px] py-4 px-6 rounded-xl">
+            suggested products:
+            <Product />
+            <Product />
+            <Product />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
